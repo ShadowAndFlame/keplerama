42a02ff8812dd0226945d6faa5d10b5db49e644b
Jonathan Orme <jonathan.orme@quakercolor.com>
2026-04-08 10:54:18 -0400

    Add simple main script

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from newton import *
+from plot import *
+from PyQt6.QtWidgets import QApplication
+
 planet_mass = 5.9722e24 # kg
 planet_diameter = 1.2756e7 # m
 planet_radius = planet_diameter/2 # m
@@ -6,11 +10,16 @@ satellite_altitude = 4e5 # m
 satellite_position = (satellite_altitude + planet_radius)*1j # m
 satellite_velocity = 7.67e3 # m/s
 
-time_step = 60 # s
-max_time = 1e6 # s
+start_time = 0 # s
+timestep = 60 # s
+max_time = 60*60*12 # s
+step_count = int(max_time / timestep)
 
 def main():
-    pass
-
+    app = QApplication([])
+    t, x, v, a = loop(start_time, satellite_position, satellite_velocity, timestep, step_count, planet_mass)
+    plot_complex(x)
+    app.exec()
+    
 if __name__ == "__main__":
     main()
