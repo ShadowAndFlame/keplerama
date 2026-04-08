f700437c44f508d43487aac4a7bbeda7d238666e
Jonathan Orme <jonathan.orme@quakercolor.com>
2026-04-08 16:10:14 -0400

    Add sample velocities to main

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ planet_mass = 5.9722e24 # kg
 planet_diameter = 1.2756e7 # m
 planet_radius = planet_diameter/2 # m
 
-satellite_altitude = 4e5 # m
-satellite_position = (satellite_altitude + planet_radius)*1j # m
-satellite_velocity = 7.67e3 # m/s
+satellite_altitude: float = 4e5 # m
+satellite_position: complex = (satellite_altitude + planet_radius)*1j # m
+satellite_velocity: float = 7.67e3 # m/s ~spherical
+satellite_velocity: float = satellite_velocity * 1.25 # elliptical
+# satellite_velocity: float = satellite_velocity * 1.45 # hyperbolic
+# satellite_velocity: float = satellite_velocity * 0.982 # crash
 
 start_time = 0 # s
 timestep = 15 # s
