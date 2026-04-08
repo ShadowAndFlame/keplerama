67dc22e5db9af8507934b250d2bd2bbe1f2feae8
Jonathan Orme <orme.jonathan@gmail.com>
2026-04-07 21:46:41 -0400

    Add inital files and values

---
+++ main.py
@@ -0,0 +1,16 @@
+planet_mass = 5.9722e24 # kg
+planet_diameter = 1.2756e7 # m
+planet_radius = planet_diameter/2 # m
+
+satellite_altitude = 4e5 # m
+satellite_position = (satellite_altitude + planet_radius)*1j # m
+satellite_velocity = 7.67e3 # m/s
+
+time_step = 60 # s
+max_time = 1e6 # s
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ newton.py
@@ -0,0 +1,4 @@
+G = 6.67430e-11 # m^3/kgs^2
+
+def g(m,r):
+    return G*m/r^2
